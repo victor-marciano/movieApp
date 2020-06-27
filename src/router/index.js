49d4c10e27a46f837349a711776f382aecb90101
victor-marciano <vpm.marciano@gmail.com>
2020-06-27 17:19:35 -0300

    set default entry page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    redirect: '/popular'
+  },
+  {
     path: '/popular',
     name: 'popular',
     component: () => import('../views/Home.vue'),
